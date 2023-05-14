c3c6d4f264bc12e4bb5606c83b8a0c3ebc330ba2
Merve Nur Tasan <127309624+mervenurtasann@users.noreply.github.com>
2023-05-14 22:50:26 +0300

    add

---
+++ src/test/java/practice/P11_Odev.java
@@ -0,0 +1,39 @@
+package practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import urilities.TestBase;
+
+import java.time.Duration;
+
+public class P11_Odev extends TestBase {
+    // ilgili kurulumlari tamamlayalim
+
+    @Test
+    public void test01() {
+        // Kullanici https://www.google.com adresine gider
+        driver.get("https://www.google.com");
+
+        // Çıkıyorsa Kullanici cookies i kabul eder
+        // Arama Kutusuna karşilastirma yapmak istedigi para birimlerini girer
+        WebElement aramaKutusu = driver.findElement(By.xpath("//textarea[@class='gLFyf']"));
+        aramaKutusu.sendKeys("usd to tl");
+
+        // Para birimlerinin karsilastirmasini alin
+        aramaKutusu.submit();
+        WebElement sonuc = driver.findElement(By.xpath("//span[@class='DFlfde SwHCTb']"));
+        String actualSonuc = sonuc.getText().replaceAll("\\D", "");
+        double actualSonucc = Double.parseDouble(actualSonuc);
+        double expectedSonuc = 1.5;
+
+
+        // Karsilastirilacak olan para biriminin 1.5 den kUCUk oldygu test edilir
+        Assert.assertTrue(actualSonucc < expectedSonuc);
+    }
+}
